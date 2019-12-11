8bc5f92fe242ed4ab0ad2432c149c8848650f3c8
LoganneF <loganne.featherston@gmail.com>
2019-12-10 19:26:27 -0800

    created loop.js file

---
+++ w01d3_homework/loop.js
@@ -0,0 +1,44 @@
+// #1
+
+/*for (let i = 0; i <=99; i++) {
+    console.log(i);
+}*/
+
+// #2
+
+/*for (let i = 99; i >= 0; i--) {
+    console.log(i);
+}*/
+
+// #3
+
+/*for (let i = 0; i <= 98; i+=2) {
+    console.log(i);
+}*/
+
+// #4
+
+/*for (let i = 98; i >= 0; i-=2) {
+    console.log(i);
+}*/
+
+// #5
+//get help here 
+
+// #6
+//get help here 
+
+// #7
+
+/*for (let i =49; i <= 72; i++) {
+    console.log(i);
+}*/
+
+// #8
+
+/*for (let i = 81; i >= 0; i--) {
+    console.log(i);
+}*/
+
+// #9
+//  get help here 
