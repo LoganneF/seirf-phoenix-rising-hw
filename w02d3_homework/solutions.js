c3ab281d92b8ab457d08f4a81fa12d37e0b70883
LoganneF <loganne.featherston@gmail.com>
2019-12-15 16:55:49 -0800

    verbal questions completed

---
+++ w02d3_homework/solutions.js
@@ -0,0 +1,25 @@
+////////////////////////
+//// Verbal Questions///
+////////////////////////
+/* 
+What is the difference between a parameter and an argument?
+- A parameter is a variable name declared within a function.
+Arguments are the data passed into the function.
+
+Within a function, what is the difference between return and console.log?
+- Return is used for returning values from a function. 
+Console.log logs out information in console.
+*/
+////////////////////////
+////// Palindrome //////
+////////////////////////
+
+
+////////////////////////
+////// Sum Array ///////
+////////////////////////
+
+
+////////////////////////
+////  Prime Numbers ////
+////////////////////////
