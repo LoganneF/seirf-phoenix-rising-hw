e9be3fcbb9b6dcb78409ac5f249c02b0f649241a
LoganneF <loganne.featherston@gmail.com>
2019-12-15 17:47:08 -0800

    sum array completed

--- w02d3_homework/solutions.js
+++ w02d3_homework/solutions.js
@@ -13,19 +13,26 @@ Console.log logs out information in console.
 ////////////////////////
 ////// Palindrome //////
 ////////////////////////
-
+/*
 const checkPalindrome = (str) => {
     return str == str.split('').reverse().join('');
 }
 
 console.log(checkPalindrome("racecar"));
-
+*/
 ////////////////////////
 ////// Sum Array ///////
 ////////////////////////
 
+const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+       sum += arr[i];
+      }
+      return sum;
 
-
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 ////////////////////////
 ////  Prime Numbers ////
 ////////////////////////
