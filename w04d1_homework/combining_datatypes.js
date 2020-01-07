6cfb814394709f9eea2ded2ea6a82d5f9b7d3a9a
LoganneF <loganne.featherston@gmail.com>
2020-01-07 14:54:43 -0800

    Combine objects, arrays, and functions more than one level deep

--- w04d1_homework/combining_datatypes.js
+++ w04d1_homework/combining_datatypes.js
@@ -1,3 +1,6 @@
+//========================================
+// Combine objects, arrays, and functions
+//=========================================
 let crayonBox = {
     crayons: ["Brick", "Scarlet", "Apricot"],
     count: 120,
@@ -12,4 +15,27 @@ let receipt = ["Walmart", "Target", costco = {guac: 4.99, cheese: 6.99}]
 //console.log(receipt[2].guac)
 
 let apartmentBuilding = [tenants= ["Loganne", "Morgan", "John"], "Quail Ridge", "Greenbridge"]
-console.log(apartmentBuilding[0][0])
+//console.log(apartmentBuilding[0][0])
+
+
+//==================================================================
+// Combine objects, arrays, and functions more than one level deep
+//==================================================================
+let project = {item: "scarf", size : "6ft"}
+
+const knit = () => {
+    console.log(project.item)
+}
+//knit();
+const crayonSelector = () => {
+    console.log(crayonBox.crayons[0])
+}
+//crayonSelector();
+const options = (message) => {
+    console.log(message)
+}
+const powerButton = (options) => {
+    return options
+}
+
+options("select a song");
