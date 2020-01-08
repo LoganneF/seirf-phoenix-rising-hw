70f4229e40be22a19cc4d935c8d15b4286bcf1c1
LoganneF <loganne.featherston@gmail.com>
2020-01-07 19:14:15 -0800

    submitting final

--- w04d1_homework/combining_datatypes.js
+++ w04d1_homework/combining_datatypes.js
@@ -37,5 +37,77 @@ const options = (message) => {
 const powerButton = (options) => {
     return options
 }
+//options("select a song");
 
-options("select a song");
+//=================================================================
+// Model a Vending Machine 
+//=================================================================
+const vendingMachine = {
+    snacks: [
+        Chips = {name: "Cheetos", price: "$1"}, 
+        Crackers = {name: "Peanut Butter", price: '$1'}, 
+        Gum = {name: "Trident", price: "$1"}
+    ],
+    vend (input) {
+        console.log('vending', vendingMachine.snacks[input]);
+    }
+};
+
+//vendingMachine.vend(0);
+
+
+
+
+//=========================================
+// Callbacks
+//=========================================
+//Make a function add that takes two arguments (numbers) and sums them together
+     let add = (num1, num2) => num1 + num2;
+     //console.log(add(5, 10))
+// Make a function subtract that takes two arguments (numbers) and subtracts them
+    let subtract = (num1, num2) => num1 - num2;
+    //console.log(subtract(5, 10))
+// Make a function multiply that takes two arguments and multiplies them
+    let multiply = (num1, num2) => num1 * num2;
+// Make a function divide that takes two arguments and divides them
+    let divide = (num1, num2) => num1 / num2;
+// Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback)
+    let calculate = (num1, num2, operates) => {
+        return operates(num1, num2); 
+    }
+    // console.log(calculate(10, 5, add))
+    // console.log(calculate(10, 5, subtract))
+    // console.log(calculate(10, 5, divide))
+    // console.log(calculate(10, 5, multiply))
+
+// Make it so that when calculate is invoked, the callback "operates" on the numbers and returns the value.
+// Call calculate 4 times, each time using one of the operation functions you wrote
+
+
+//=================================================================
+// Function definition placement
+//=================================================================
+
+
+// const bar = () => {
+//     console.log('bar here');
+// }
+
+// const foo = () => {
+//     console.log('foo here');
+// }
+
+//bar();
+//foo();
+
+//=================================================================
+// Error Reading 
+//=================================================================
+
+//this needs to be declared after the function
+foo();
+
+//missing '=' after foo
+const foo =()=>{
+    console.log('hi');
+}
