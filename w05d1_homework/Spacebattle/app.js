6b7ee0ed05b8e19336032038fc878598fd46aeab
LoganneF <loganne.featherston@gmail.com>
2020-01-11 11:47:05 -0800

    created attacking game logic

--- w05d1_homework/Spacebattle/app.js
+++ w05d1_homework/Spacebattle/app.js
@@ -4,7 +4,7 @@
 //================
 
 /*
-1. CREATE ACTOR OBJECT FOR MY SHIP:
+1. CREATE PLAYER'S SHIP:
     - USS SCHWARZENEGGER SPACESHIP
         -HULL(HITPOINTS): 20; 0 = DESTROYED
         -FIREPOWER: 5;
@@ -14,72 +14,68 @@
         -GET TO ATTACK FIRST
         -CAN ONLY ATTACK ONE AT A TIME
 */
-let ussSchwarz = {
+let playerShip = {
     hull: 20,
     firepower: 5,
     accuracy: .7,
     //add methods for battle here
-    attack: () => {
+    attack: enemyShip => {
        console.log("Attacking")
-       //calculate chance attack will damage other ship w Math.random
+
+       //check for hit success based on playerShip accuracy
+       if(Math.random() < playerShip.accuracy) {
+           console.log("Attack Hit!")
+        enemyShip.hull -= playerShip.firepower;
+       } else {
+           console.log("Attack Missed!");
+       }
+            console.log(enemyShip);
+
+ //calculate chance attack will damage other ship w Math.random
+       //if (Math.random() < alien[0].accuracy) {
+           //console.log('Youve been hit!');
+      // }
+      
     }
 };
 
 
-/*
-2. CREATE CLASS FOR SIX ALIEN SPACESHIPS
-        -SIX ALIEN SPACESHIPS
-The alien ships should each have the following ranged properties determined randomly:
-        -HULL(HITPOINTS): BETWEEN 3 AND 6; 0 = DESTROYED
-        -FIREPOWER: BETWEEN 2 AND 4;
-        -ACCURACY: BETWEEN .6 AND .8;
-        -ATTACK METHOD
-        -LOOPS FOR ATTACKING EACH OTHER UNTIL ONE DESTROYED
-        -WEAKNESS= ATTACK ONE AT A TIME
-*/
-class Alien {
-    constructor(hull, firepower, accuracy, attack)
-    this.hull
-    this.firepower
-    this.accuracy
+//CREATE CLASS FOR SIX ALIEN SPACESHIPS
+        
+class AlienShip {
+    constructor() {
+    //TODO: make hull random bw 3 and 6
+    this.hull = 3
+    //TODO: make firepower random bw 2 and 4
+    this.firepower = 2
+    //TODO: make acccuuracy random bw .6 and .8
+    this.accuracy = 0.6
 }
-
-//CREATE A GAME OBJECT
-let game = {
-    hero: ussSchwarz,
-    villain: Alien,
-
+attack(){
+    console.log("Alien ship atttacking")
 }
-enemyShip = {
-shipNum: 1,
-health: 100,
-isAttacking:false
 }
+//START THE GAME
+console.log("generating enemy ships")
+enemy = new AlienShip();
+console.log(enemy);
 
-const attackEnemy = (enemyShip) => {
-
-    console.log("Boom! you have been hit");
-    enemyShip.health -= 10;
-}
 
 //HIT IS BASED OFF OF ACCURACY. MATH.RANDOM ACCURACY TO DETERMINE IF YOU MET THRESHOLD TO HIT THE ALIEN OR IF THEY HIT YOU 
 // HAVE BUTTON REP SHIPS. IF BUTTON CLICKED EQUAL ATTACK, THIS SETS OFF HIT FUNCTION 
+
+
+//CREATE A GAME OBJECT
+// END GAME LOGIC
+//  - player wins when alien.hull = 0
+
+// TURN BY TURN LOGIC
+playerShip.attack(enemy);
+
+// BATTLE
+
+// RETREAT OPTION 
 /*
-START THE GAME
-//WHen USS schwartz clicked, signal user to click alien button 
-
-
-END GAME LOGIC
- - player wins when alien.hull = 0
-
-TURN BY TURN LOGIC
- - if ussSwartz shoots & alien != hit, then alien turn
- -ussS
-
-BATTLE
-
-RETREAT OPTION 
-
 . GAME ROUND .
 
 You attack the first alien ship
@@ -100,4 +96,17 @@ You win the game if you destroy all of the aliens.
 
 You lose the game if you are destroyed.
 
+*/
+/*
+enemyShip = {
+shipNum: 1,
+health: 100,
+isAttacking:false
+}
+
+const attackEnemy = (enemyShip) => {
+
+    console.log("Boom! you have been hit");
+    enemyShip.health -= 10;
+}
 */
