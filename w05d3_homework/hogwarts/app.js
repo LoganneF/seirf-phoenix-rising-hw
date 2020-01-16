ff12a82fd13edf0b4cdf601148a5b56c56d1e8b5
LoganneF <loganne.featherston@gmail.com>
2020-01-16 04:44:11 -0800

    Commit 3-code not very dry

--- w05d3_homework/hogwarts/app.js
+++ w05d3_homework/hogwarts/app.js
@@ -11,9 +11,29 @@ $(() => {
 	const $house = $('<h3>').text('RavenClaw');
 	const $pet = $('<h4>').addClass('cat').text('Twitch');
 	const $wand = $('<h4>').text('Elder Wand');
+
+	$container.append($name);
+	$container.append($house);
+	$container.append($pet);
+	$container.append($wand);
+
+	const $trunk = $('<ul>').attr('storage', 'trunk');
+	//li items
+	const $butterBeer = $('<li>').text('butter beer');
+	const $cloak = $('<li>').text('invisibility cloak').addClass('secret');
+	const $map = $('<li>').text('magic map').addClass('secret');
+	const $timeTurner = $('<li>').text('time turner').addClass('secret');
+	const $leash = $('<li>').text('leash').addClass('cat');
+	const $candy = $('<li>').text('Bertie Botts Every Flavored Beans');
 	
+	$trunk.append($butterBeer);
+	$trunk.append($cloak);
+	$trunk.append($map);
+	$trunk.append($timeTurner);
+	$trunk.append($leash);
+	$trunk.append($candy);
 
-
+	$container.append($trunk);
 
 
 
