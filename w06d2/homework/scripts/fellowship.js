20741394c383add59d990519ef4b2bc917026034
LoganneF <loganne.featherston@gmail.com>
2020-01-21 14:45:44 -0800

    Chapter 9 complete - Updated Gandalf

--- w06d2/homework/scripts/fellowship.js
+++ w06d2/homework/scripts/fellowship.js
@@ -231,15 +231,16 @@ forgeTheFellowShip();
 // Chapter 9
 // ============
 const theBalrog = () => {
-
+   console.log("the Balrog");
+   $( () => {
   // 1. change the 'Gandalf' text to 'Gandalf the White'
-
   // 2. add a class "the-white" to this element
-
+  $('.buddy:first').text('Gandalf the White').addClass("the-white");
+  
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
-
+});
 };
-
+theBalrog();
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 9 complete - Updated Gandalf"
 
