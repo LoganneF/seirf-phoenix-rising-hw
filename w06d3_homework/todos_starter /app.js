1aa643b9434a6590854a03df5c4bca95571c2437
LoganneF <loganne.featherston@gmail.com>
2020-01-26 16:45:01 -0800

    accepting user input and showing teal boxes on to do column

--- w06d3_homework/todos_starter /app.js
+++ w06d3_homework/todos_starter /app.js
@@ -1,19 +1,39 @@
 $( () => {
-//when user submits data, a <div class="to-do-item"> is created
+
+//=====================
+//  Big Picture 
+//=====================
+//when user submits data, a div item is created
 //that data is stored and displayed in things to do column
-
 //when user clicks 'completed' button, list item is moved to things that are done column
-
 //when user clicks 'remove' button, it clears it from page 
+
+
+//=======================
+//  Step by Step
+//=======================
+
+//create todo items array
 let toDoItemsArr = []
 
+//create function to create todo divs
 const createToDoItem = () => {
+    //log user input
     console.log($('#input-box').val());
 
-    $newToDoItem = $('<div>').text($('#input-box').val()).addClass("#to-do-item");
+    //create todo item div from user input <div class="to-do-item"> 
+    $newToDoItem = $('<div>').text($('#input-box').val()).addClass("to-do-item");
+    //push new div into array
     toDoItemsArr.push($newToDoItem);
+
+    //append user todo items array to ToDo List column
+    $('#to-do-list').append(toDoItemsArr)
+    //prevent page from refreshing
+    event.preventDefault();
 }
 
+//user clicking submit button activates function 
+$('#submit').on('click', createToDoItem)
 
 
 });
