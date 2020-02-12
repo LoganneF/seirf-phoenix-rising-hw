f11178f0604f1a8f127cd7dc28a56361444fc577
LoganneF <loganne.featherston@gmail.com>
2020-02-12 14:01:25 -0800

    homework done

--- w09d2_homework/pass-it-around/server.js
+++ w09d2_homework/pass-it-around/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get("/:bugNumber", (req, res)=> {
+    let number = parseInt(req.params.bugNumber) - 1
+    
+    if(req.params.bugNumber > 0) {
+        res.send(`<h1>${req.params.bugNumber} bugs in the code</h1>
+        <a href="${number}">take one down, patch it around`);
+    }
+})
 app.get('/', (req, res)=>{
-    res.send(`99 bottles of beer on the wall. <a href='/98'>Take one down, pass it around`);
+    res.send(`99 bugs in the code. <a href='/98'>Take one down, patch it around`);
 })
 app.listen(port, ()=>{
     console.log('listening on port ' , port);
