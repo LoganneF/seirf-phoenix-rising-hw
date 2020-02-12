ec7fe95da12f7186424fa79dc83a8735734aece7
LoganneF <loganne.featherston@gmail.com>
2020-02-12 13:25:47 -0800

    Tip Calculator express application created.

--- w09d2_homework/server.js
+++ w09d2_homework/server.js
@@ -6,6 +6,11 @@ app.get('/greeting/:name', (req, res)=>{
     res.send("Hello " + req.params.name);
   });
 
+  app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    let tip = parseInt(req.params.total) * parseInt(req.params.tipPercentage)/100
+    res.send(tip);
+  });
+
 
 app.listen(port, ()=>{
   console.log('listening on port ' , port);
