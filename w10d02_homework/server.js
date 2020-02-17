e46b825629eb38b13f3aef9c151a1b2b3736830a
LoganneF <loganne.featherston@gmail.com>
2020-02-16 17:44:34 -0800

    loads table with data on page load. needs css still

--- w10d02_homework/server.js
+++ w10d02_homework/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const budget = require('./models/budget.js')
 
 //configure express.static
 app.use(express.static('public'));
@@ -12,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/budget', (req, res)=>{
     //res.send(budget);
-    res.render('index.ejs')
+    res.render('index.ejs', {budget: budget})
 });
 
 // Load up the express web server. IMPORTANT: Always do this at the end of your server.js:
