94befc28404a2f54bce457da4e9b0e5a05da2f20
LoganneF <loganne.featherston@gmail.com>
2020-02-17 18:11:17 -0800

    show route is working

--- w10d02_homework/server.js
+++ w10d02_homework/server.js
@@ -11,11 +11,17 @@ app.get('/', (req, res) => {
     res.send("working!");
 });
 
+//index route
 app.get('/budget', (req, res)=>{
     //res.send(budget);
     res.render('index.ejs', {budget: budget})
 });
 
+//show route
+app.get('/budget/:id', (req, res)=>{
+    const currentItem = budget[req.params.id]
+    res.render('show.ejs', {budget: currentItem})
+})
 // Load up the express web server. IMPORTANT: Always do this at the end of your server.js:
 app.listen(port,() => {
     console.log('listening on port' , port);
