44613558535abd2629813b289c21940d6921ae55
LoganneF <loganne.featherston@gmail.com>
2020-03-02 10:49:07 -0800

    all seven routes are up

--- w11d3_homework/server.js
+++ w11d3_homework/server.js
@@ -27,9 +27,37 @@ app.get("/products", (req, res) => {
   // });
 });
 
-//show route
+//show route (NEED MORE HERE)
 app.get('/products/:id', (req, res)=>{
   res.render('show.ejs')
+});
+
+//new route (NEED MORE HERE)
+app.get('/products/new', (req, res)=> {
+  res.render('new.ejs');
+});
+
+//create route (NEED MORE HERE)
+// app.post('/products', (req, res)=>{
+//   //let index = products.push(req.body) -1;
+//   //res.redirect(`/products/${index}`);
+// });
+
+//edit route (NEED MORE HERE)
+app.get('/products/:id/edit', (req, res)=>{
+  res.render('edit.ejs')
+});
+
+//update route (NEED MORE HERE)
+app.put('products/:id', (req, res)=>{
+  //products[req.params.id] = req.body;
+  res.redirect('/products');
+});
+
+//delete route (NEED MORE HERE)
+app.delete('products/:id', (req, res)=>{
+  //products.splice(req.params.id, 1);
+  res.redirect('/products');
 });
 
   // Web server:
