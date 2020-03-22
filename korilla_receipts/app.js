52491ae34ca5969d15e1729e407eecd73dab724a
LoganneF <loganne.featherston@gmail.com>
2020-03-21 20:53:14 -0700

    moved lab to homework

---
+++ korilla_receipts/app.js
@@ -0,0 +1,9 @@
+class App extends React.Component {
+    render () {
+      return (
+        <h1> React Tac Toe </h1>
+      )
+    }
+  }
+  
+  ReactDOM.render(<App />, document.querySelector('.main'));
