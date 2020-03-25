340bdb077d1305f6b9e560b88c242c0501dd7cde
LoganneF <loganne.featherston@gmail.com>
2020-03-24 20:17:28 -0700

    added handleSubmit function

--- Unit 3/random_taco/app.js
+++ Unit 3/random_taco/app.js
@@ -1,10 +1,28 @@
 class App extends React.Component {
     state = {
-        baseURL: 'http://taco-randomizer.herokuapp.com/random/?full-tack=true'
+        baseURL: 'http://taco-randomizer.herokuapp.com/random/?full-tack=true/?',
+       
     }
+
+    handleSubmit (event) {
+        event.preventDefault()
+        this.setState(
+            {
+            fetchURL: this.state.baseURL
+        }, () => {
+            console.log(this.state.fetchURL)
+            fetch("http://taco-randomizer.herokuapp.com/random/?full-tack=true")  
+                .then(response => {
+                    return response.json()
+            }).then(json => this.setState({
+                taco: json
+            }),
+                err => console.log(err))
+        })
+    };
     render () {
         return (
-            <div>Hello World</div>
+            <div>Random Taco</div>
         )
     }
 }
