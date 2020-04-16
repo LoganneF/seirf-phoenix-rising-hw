29192dcc5daf2f1a42debc0486f9f29d202eb7d7
LoganneF <loganne.featherston@gmail.com>
2020-04-16 15:10:35 -0700

    starting blackjack hw

---
+++ Unit 4/w18d3/blackjack.rb
@@ -0,0 +1,15 @@
+class Person 
+    def initialize name
+        @name = name
+    end
+
+    def name
+        @name
+    end
+end
+
+class The_House
+    def initialize name
+    def name
+        @name
+    end
